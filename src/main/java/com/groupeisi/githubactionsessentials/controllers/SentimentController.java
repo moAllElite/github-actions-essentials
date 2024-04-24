efde36400e556f4aa84ae0a0dd60bbe63cb2c88d
ahmed <mouhamedn347@gmail.com>
2024-04-24 14:29:59 +0000

    crud-sentiments

---
+++ src/main/java/com/groupeisi/githubactionsessentials/controllers/SentimentController.java
@@ -0,0 +1,39 @@
+package com.groupeisi.githubactionsessentials.controllers;
+
+import com.groupeisi.githubactionsessentials.dto.SentimentDTO;
+import com.groupeisi.githubactionsessentials.services.ISentimentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@AllArgsConstructor
+@RequestMapping(value = "/sentiments",produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
+public class SentimentController {
+    private  ISentimentService service;
+    @GetMapping("info")
+    public ResponseEntity<String> show() {
+        return ResponseEntity.ok("Hello World this is Mouhamed Niang the great !!!");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SentimentDTO>> getSentiments() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SentimentDTO> getSentiment(@PathVariable Long id) {
+        return  ResponseEntity.ok(service.findById(id));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createSentiment(@RequestBody SentimentDTO sentimentDTO) {
+        service.save(sentimentDTO);
+    }
+
+
+}
